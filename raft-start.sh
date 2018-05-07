3200829b1f486dafd5bbf9fdb1db77d41ba6ae9f
vasa-develop <vasa.develop@gmail.com>
2018-05-07 16:08:11 +0530

    added 3rd node

--- raft-start.sh
+++ raft-start.sh
@@ -15,4 +15,4 @@ set +v
 
 echo
 echo "All nodes configured. See 'qdata/logs' for logs, and run e.g. 'geth attach qdata/dd1/geth.ipc' to attach to the first Geth node."
-echo "To test sending a private transaction from Node 3 to Node 7, run './runscript.sh script1.js'"
+echo "To test sending a private transaction, run './runscript.sh script1.js'"
--- runscript.sh
+++ runscript.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-PRIVATE_CONFIG=qdata/c1/tm.ipc geth --exec "loadScript(\"$1\")" attach ipc:qdata/dd1/geth.ipc
+PRIVATE_CONFIG=qdata/c4/tm.ipc geth --exec "loadScript(\"$1\")" attach ipc:qdata/dd4/geth.ipc
